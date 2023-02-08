0fb32728badf7f9d748ab5af9f943e45e020d742
chela-droid <[200~chelangatcaroline70@gmail.com~>
2023-02-09 01:49:13 +0300

    commit tree c5911657e2940dffd973004a043db7e6ec08c9ec

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :genre
 
+    def initialize(genre,title="And Then There Were None",author="Agatha Christie")
+        @title=title
+        @author=author
+        @genre=genre
+    end
+    def page_count=(pages)
+        @page_count= pages
+    end
+    def page_count
+        @page_count
+    end
+    def turn_page
+        print "Flipping the page...wow, you read fast!"
+    end
+    
+end  
 
