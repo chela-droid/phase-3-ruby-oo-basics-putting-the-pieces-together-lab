51f8044427a588e4e4e90c809ba72a5957bc241b
chela-droid <[200~chelangatcaroline70@gmail.com~>
2023-02-09 09:00:43 +0300

    pieces together

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+    def initialize( brand, color="red", size=9.5, material="suede", condition="tattered")
+        @brand=brand
+        @color=color
+        @size=size
+        @material=material
+        @condition=condition
+    end
+    def cobble (state="Your shoe is as good as new!")
+        puts state
+        @condition ='new'
+    end
+   
+
+end
